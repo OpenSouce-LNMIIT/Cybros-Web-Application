b025628fe85a88d574a0f2d9054ebc08c9203788
Chaitanya Giri <girichaitanya11@gmail.com>
2017-09-26 13:38:01 +0530

    Unique username validation and Password validation done

--- models/User.js
+++ models/User.js
@@ -2,7 +2,7 @@ var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
 var user = new Schema({
-    Name: {type:String,default:null},
+    username: {type:String,default:null},
     Email: {type:String,default:null},
     Password: {type:String,default:null},
     Repassword :{type:String,default:null}
--- routes/signup.js
+++ routes/signup.js
@@ -14,21 +14,49 @@ app.use(cookieParser());
 router.get('/', function(req, res) {
     res.render('signup.hbs', {});
 });
+//
 router.post('/new_User', function(req, res) {
-    var user = new User();
-    user.Name = req.body.username;
-    user.Email = req.body.email;
-    user.Password = req.body.password;
-    user.Repassword = req.body.repassword;
-    user.save(function(err, registeredUser){
+    // Unique user validation
+    var userlist = [];
+    User.find({username:req.body.username},function(err,user){
         if(err){
-            res.status(500).send({error:"Could not save register user"});
-            console.log("Could not save register user");
+            res.status(500).send({error:"Could not get to Database"});
+            console.log("Could get to database");
         }
         else{
-            res.send(registeredUser);
-            console.log('! A user registered: Username: ' + req.body.username + ', Password: ' + req.body.password+', Email: ' + req.body.email);            
+            if (user.length!=0) {
+                if(user[0].username){
+                    console.log("Username already exist username:"+user[0].username);
+                    res.render('signup.hbs', {
+                        passwordNotMatch:"Username already exist, try again"
+                    });                      
+                }                                 
+            }
+            else{
+                //Password validation
+                if(req.body.password == req.body.repassword){
+                    var user = new User();
+                    user.username = req.body.username;
+                    user.Email = req.body.email;
+                    user.Password = req.body.password;
+                    user.save(function(err, registeredUser){
+                        if(err){
+                            res.status(500).send({error:"Could not save register user"});
+                            console.log("Could not save register user");
+                        }
+                        else{
+                            res.send(registeredUser);
+                            console.log('! A user registered: Username:: ' + req.body.username + ', Password: ' + req.body.password+', Email: ' + req.body.email);            
+                        }
+                    });
+                } 
+                else {
+                    res.render('signup.hbs', {
+                        passwordNotMatch:"Password do not match, try again."
+                    });
+                }
+            }             
         }
-    });
+    });         
 });
 module.exports = router;
