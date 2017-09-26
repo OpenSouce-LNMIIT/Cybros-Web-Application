faa8b98cea3d909de0d43dff18e126e9d025ba46
Chaitanya Giri <girichaitanya11@gmail.com>
2017-09-26 13:38:30 +0530

    Unique username validation and Password validation done

--- models/User.js
+++ models/User.js
@@ -4,9 +4,7 @@ var Schema = mongoose.Schema;
 var user = new Schema({
     username: {type:String,default:null},
     Email: {type:String,default:null},
-    Password: {type:String,default:null},
-    Repassword :{type:String,default:null}
-
+    Password: {type:String,default:null}
 });
 
 module.exports = mongoose.model("User",user);
