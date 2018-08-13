19452b48b113537afc876b6d80c315389b0ef156
Chaitanya Giri <16ucc028@lnmiit.ac.in>
2018-08-14 01:10:44 +0530

    Update signup.js

--- routes/signup.js
+++ routes/signup.js
@@ -17,7 +17,7 @@ var transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
       user: 'girichaitanya11@gmail.com',
-      pass: '9424108852'
+      pass: 'Your password here'
     }
   });
 
